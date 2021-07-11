2dfa2ce6eedccdb3c748f273ec9986f951366ae4
Nasser M. Tabook <nmtabook@gmail.com>
2021-07-11 23:13:06 +0400

    make changes

--- nasserblog/settings.py
+++ nasserblog/settings.py
@@ -83,7 +83,7 @@ else:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogDB',
+        'NAME': 'blogdb',
         'USER': 'blog_admin',
         'PASSWORD': 'Goo@allmona_22',
         'HOST': 'localhost',
