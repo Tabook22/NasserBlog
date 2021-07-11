011ec761adef4a69ffc5d46f04532785a38c4968
Nasser M. Tabook <nmtabook@gmail.com>
2021-07-11 23:20:08 +0400

    tt

--- nasserblog/settings.py
+++ nasserblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u8pid-ns&!7!2*#nc^panic5&ji7y3w(1$rqiy5q%pn$kqw@(b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['allmona.com','165.227.92.169','localhost']
 
