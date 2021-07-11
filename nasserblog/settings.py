feab5a1596d747c5233216c3053195a2e6613102
Nasser M. Tabook <nmtabook@gmail.com>
2021-07-11 23:24:08 +0400

    dddd

--- nasserblog/settings.py
+++ nasserblog/settings.py
@@ -72,24 +72,24 @@ WSGI_APPLICATION = 'nasserblog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogdb',
-        'USER': 'blog_admin',
-        'PASSWORD': 'Goo@allmona_22',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+#if DEBUG:
+#    DATABASES = {
+#        'default': {
+ #           'ENGINE': 'django.db.backends.sqlite3',
+ #           'NAME': BASE_DIR / 'db.sqlite3',
+ #       }
+ #   }
+#else:
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'blogdb',
+    'USER': 'blog_admin',
+    'PASSWORD': 'Goo@allmona_22',
+    'HOST': 'localhost',
+    'PORT': '',
 }
+#}
 
 
 # Password validation
