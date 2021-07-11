dd3540738186310abf58705fe5e46aaa6eee0154
Nasser M. Tabook <nmtabook@gmail.com>
2021-07-11 22:43:18 +0400

    new changes

--- nasserblog/settings.py
+++ nasserblog/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'nasserblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
